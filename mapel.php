e4eaeb12295289250b2f78c3f719620f26a78e57
mukijo-r <mukijo@student.unsia.ac.id>
2023-11-03 19:51:24 +0700

    menambahkan halaman mapel

---
+++ mapel.php
@@ -0,0 +1,156 @@
+<?php
+require 'function.php';
+require 'cek.php';
+require 'config.php';
+date_default_timezone_set('Asia/Jakarta');
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Halaman Input Mapel</title>
+        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+        <link href="css/styles.css" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="sb-nav-fixed">
+        <?php include 'navbar.php'; ?>
+        <div id="layoutSidenav">
+            <?php include 'sidebar.php'; ?>
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid px-4">
+                        <h2 class="mt-4">Tambahkan Mata Pelajaran</h2>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item active">MASTER / Mata Pelajaran</li>                            
+                        </ol>                        
+                        <br>
+                        <div class="container-fluid px-4">
+                            <div class="row">
+                                <div class="col-md-2">
+                                    <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#modalTambahMapel">
+                                        Mata Pelajaran Baru
+                                    </button>
+                                </div>
+                                <div class="col-md-8">
+                                    <?php
+                                    if (isset($_SESSION['flash_message'])) {
+                                        $message_class = isset($_SESSION['flash_message_class']) ? $_SESSION['flash_message_class'] : 'alert-success';
+                                        echo '<div class="alert ' . $message_class . ' text-center">' . $_SESSION['flash_message'] . '</div>';
+                                        unset($_SESSION['flash_message']); // Hapus pesan flash setelah ditampilkan
+                                    }
+                                    
+                                    ?>
+                                </div>
+                            </div>
+                        </div>                    
+                        <br>
+                        <div class="card mb-4">
+                            <div class="card-header">
+                                <i class="fas fa-table me-1"></i>
+                                Daftar Mata Pelajaran
+                            </div>
+                            <div class="card-body">
+                                <table id="datatablesSimple">
+                                    <thead>
+                                        <tr>
+                                            <th>No.</th>
+                                            <th>Mata Pelajaran</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    <?php 
+                                    $dataNilaiMapel = mysqli_query($conn, "SELECT * FROM mapel;");
+
+                                    $i = 1;
+                                    
+                                    while($data=mysqli_fetch_array($dataNilaiMapel)){
+                                        $idMapel = $data['id_mapel'];                                       
+                                        $mapel = $data['mapel'];
+
+                                        ?>
+                                        <tr>
+                                            <td><?=$i++;?></td>
+                                            <td><?=$mapel;?></td>
+                                            <td>
+                                                <button type="button" class="btn btn-warning" name="tblEdit" data-bs-toggle="modal" data-bs-target="#modalEditMapel<?=$idMapel;?>">Edit</button>
+                                            </td>
+                                        </tr>
+
+                                        <!-- Modal edit Mata Pelajaran -->
+                                        <div class="modal fade" id="modalEditMapel<?=$idMapel;?>">
+                                            <div class="modal-dialog">
+                                                <div class="modal-content">
+                                                    <!-- Modal Header -->
+                                                    <div class="modal-header">
+                                                        <h4 class="modal-title">Ubah Mata Pelajaran</h4>
+                                                        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                                                    </div>
+                                                    <!-- Modal Body -->
+                                                    <form method="post">
+                                                        <div class="modal-body">                    
+                                                            <div class="mb-3">
+                                                                <label for="mapelEdit">Nama Mata Pelajaran :</label>                        
+                                                                <input type="text" name="mapelEdit" id="mapelEdit" value="<?=$mapel;?>" class="form-control" required>                    
+                                                        </div>
+                                                        <div class="text-center">
+                                                            <input type="hidden" name="idMapel" value="<?=$idMapel;?>">
+                                                            <button type="submit" class="btn btn-primary" name="UbahMapel">Simpan</button>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    <?php
+                                    };
+
+                                    ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="assets/demo/chart-area-demo.js"></script>
+        <script src="assets/demo/chart-bar-demo.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+        <script src="js/datatables-simple-demo.js"></script>
+        <script>
+        </script>
+    </body>   
+
+    <!-- Modal Tambah Mapel -->
+    <div class="modal fade" id="modalTambahMapel">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <!-- Modal Header -->
+                <div class="modal-header">
+                    <h4 class="modal-title">Tambah Mata Pelajaran</h4>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                </div>
+                <!-- Modal Body -->
+                <form method="post">
+                    <div class="modal-body">                    
+                        <div class="mb-3">
+                            <label for="mapel">Nama Mata Pelajaran :</label>                        
+                            <input type="text" name="mapel" id="mapel" class="form-control" required>                    
+                    </div>
+                    <div class="text-center">
+                        <button type="submit" class="btn btn-primary" name="tambahMapel">Simpan</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</html>
