6c622e5ec93d16cf1d3006f24f90266a3b31dd6c
mukijo-r <mukijo@student.unsia.ac.id>
2023-11-05 15:23:28 +0700

    mengubah judul

--- navbar.php
+++ navbar.php
@@ -20,7 +20,7 @@ require 'cek.php';
     </head>
     <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <!-- Navbar Brand-->
-            <a class="navbar-brand ps-4" href="index.php">SINILA SDK BHAKTI</a>            
+            <a class="navbar-brand ps-4" href="index.php">SINILA KUMER</a>            
             <!-- Sidebar Toggle-->
             <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
             <!-- Navbar Search-->
