0178bc9438e36170a62eacfb50903ef2733a8d5c
mukijo-r <mukijo@student.unsia.ac.id>
2025-03-12 11:03:07 +0700

    menambahkan fungsi backup database

--- sidebar_function.php
+++ sidebar_function.php
@@ -38,8 +38,6 @@ if(isset($_POST['tambahTahunAjar'])){
     }
 }
 
-
-
 //Ubah Kelas
 if(isset($_POST['ubahKelas'])){
     $newKelas = $_POST['kelas'];
@@ -50,6 +48,108 @@ if(isset($_POST['ubahKelas'])){
 if(isset($_POST['ubahTahunAjar'])){
     $tahun_ajar = $_POST['tahunAjar'];
     $_SESSION['tahunAjar'] = $tahun_ajar;
+}
+
+//Simpan Backup
+if(isset($_POST['simpanBackup'])){
+    // $nama_file = $_POST['fileBackup'];
+
+    // // Lakukan pemeriksaan panjang dan format sebelum menjalankan kueri
+    // if (strlen($nama_file) >= 1) {
+        $database = 'sdk';
+        $user = 'root';
+        $pass = '';
+        $host = 'localhost';
+        $charset = "utf8mb4"; # utf8mb4_unicode_ci 
+
+        $conn = new mysqli($host, $user, $pass, $database);
+        $conn->set_charset($charset);
+
+        # get all tables
+        $result = mysqli_query($conn, "SHOW TABLES");
+        $tables = array();
+
+        while ($row = mysqli_fetch_row($result)) {
+            $tables[] = $row[0];
+        }
+
+        # Get tables data 
+        $sqlScript = "";
+        foreach ($tables as $table) {
+            $query = "SHOW CREATE TABLE $table";
+            $result = mysqli_query($conn, $query);
+            $row = mysqli_fetch_row($result);
+            
+            $sqlScript .= "\n\n" . $row[1] . ";\n\n";
+            
+            
+            $query = "SELECT * FROM $table";
+            $result = mysqli_query($conn, $query);
+            
+            $columnCount = mysqli_num_fields($result);
+            
+            for ($i = 0; $i < $columnCount; $i ++) {
+                while ($row = mysqli_fetch_row($result)) {
+                    $sqlScript .= "INSERT INTO $table VALUES(";
+                    for ($j = 0; $j < $columnCount; $j ++) {
+                        $row[$j] = $row[$j];
+                        
+                        $sqlScript .= (isset($row[$j])) ? '"' . $row[$j] . '"' : '""';
+
+                        if ($j < ($columnCount - 1)) {
+                            $sqlScript .= ',';
+                        }
+
+                    }
+                    $sqlScript .= ");\n";
+                }
+            }
+            
+            $sqlScript .= "\n"; 
+        }
+
+        //save file
+        // Tentukan folder tempat penyimpanan backup (misalnya di /var/backups/)
+        $folder = 'C:/var/backups/';  // Ganti dengan folder yang diinginkan
+
+        // Periksa apakah folder ada, jika belum, buat folder tersebut
+        if (!is_dir($folder)) {
+            mkdir($folder, 0777, true);  // Membuat folder jika belum ada
+        }
+
+        // Generate nama file backup dengan timestamp
+        $time = time();
+
+        // Tentukan path lengkap untuk file backup
+        $file_path = $folder . 'backup_' . time() . '.sql';
+
+        // Simpan file backup ke dalam folder tersebut
+        $mysql_file = fopen($file_path, 'w+');
+        fwrite($mysql_file, $sqlScript);
+        fclose($mysql_file);
+
+        $sweetAlert = "";
+        if ($mysql_file) {
+            $sweetAlert = "Swal.fire({
+                title: 'Sukses!',
+                text: 'File backup disimpan di C:/var/backups/',
+                icon: 'success',
+                timer: 2000,
+                timerProgressBar: true,
+                showConfirmButton: false
+            });";
+        } else {
+            $sweetAlert = "Swal.fire({
+                title: 'Gagal!',
+                text: 'Simpan backup gagal.',
+                icon: 'error',
+                timer: 2000,
+                timerProgressBar: true,
+                showConfirmButton: false
+            });";
+        }
+
+    // }
 }
 
 
